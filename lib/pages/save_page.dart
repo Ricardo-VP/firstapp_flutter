b4c4ca34000dcbc6de7b22259a60138276787cea
Ricardo-VP <ricardo.vacape@gmail.com>
2022-05-30 00:59:39 -0500

    Adding the save page form 😁

--- lib/pages/save_page.dart
+++ lib/pages/save_page.dart
@@ -9,7 +9,61 @@ class SavePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Save")),
-      body: const Text("Save page"),
+      body: _FormSave(),
+    );
+  }
+}
+
+class _FormSave extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Insert the title";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: "Title",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              maxLines: 4,
+              maxLength: 1000,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Insert the content";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: "Content",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    print('Save');
+                  }
+                },
+                child: Text("Save"))
+          ],
+        ),
+      ),
     );
   }
 }
